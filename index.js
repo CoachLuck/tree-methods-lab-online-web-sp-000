9a7cd29edb34da4c43231d47e382e8b63317e332
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-19 09:18:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-let rootNode = {data: 5, left: null, right: null}
-let firstNewNode = {data: 3, left: null, right: null}
-let secondNewNode = {data: 7, left: null, right: null}
-let thirdNewNode = {data: 9, left: null, right: null}
-
 function findOrAdd(node, newNode){
   if(node.data == newNode.data){
     return true
@@ -29,15 +24,10 @@ function inOrder(node) {
   if (node.left) {
     inOrder(node.left)
   }
+  
   console.log(node.data)
+  
   if(node.right) {
     inOrder(node.right)
   }
 }
-
-findOrAdd(rootNode, firstNewNode)
-findOrAdd(rootNode, secondNewNode)
-findOrAdd(rootNode, thirdNewNode)
-let result = findOrAdd(rootNode, thirdNewNode)
-console.log(rootNode)
-console.log("Result: ", findOrAdd(rootNode, thirdNewNode))
